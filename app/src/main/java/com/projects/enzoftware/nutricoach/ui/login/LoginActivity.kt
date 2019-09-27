c502c9bbb2d60b8233e38035772efe2c128cd62f
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-27 01:19:52 -0500

    add login view and presenter

--- app/src/main/java/com/projects/enzoftware/nutricoach/ui/login/LoginActivity.kt
+++ app/src/main/java/com/projects/enzoftware/nutricoach/ui/login/LoginActivity.kt
@@ -3,11 +3,21 @@ package com.projects.enzoftware.nutricoach.ui.login
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.projects.enzoftware.nutricoach.R
+import com.projects.enzoftware.nutricoach.ui.login.view.LoginView
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    override fun onLoginSuccess() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLoginFail() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
---
+++ app/src/main/java/com/projects/enzoftware/nutricoach/ui/login/presenter/LoginPresenter.kt
@@ -0,0 +1,12 @@
+package com.projects.enzoftware.nutricoach.ui.login.presenter
+
+import com.projects.enzoftware.nutricoach.ui.base.BasePresenter
+import com.projects.enzoftware.nutricoach.ui.login.view.LoginView
+
+class LoginPresenter : BasePresenter<LoginView?>(){
+
+    fun login(email: String, password: String){
+
+    }
+
+}
