71df682692cdc75fa223753d78edc5c6d45af67d
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-27 12:07:20 -0500

    add splash activity

--- app/src/main/java/com/projects/enzoftware/nutricoach/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/projects/enzoftware/nutricoach/ui/splash/SplashActivity.kt
@@ -1,14 +1,22 @@
 package com.projects.enzoftware.nutricoach.ui.splash
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.projects.enzoftware.nutricoach.R
+import com.projects.enzoftware.nutricoach.ui.onBoarding.OnBoardingActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            startActivity(Intent(this, OnBoardingActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
