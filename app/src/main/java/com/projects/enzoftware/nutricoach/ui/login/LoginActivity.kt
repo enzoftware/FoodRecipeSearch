3a34f5c03c06fbe38543097c70a7cc24a3ff1aea
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-27 12:07:10 -0500

    add login & register ui

--- app/src/main/java/com/projects/enzoftware/nutricoach/ui/login/LoginActivity.kt
+++ app/src/main/java/com/projects/enzoftware/nutricoach/ui/login/LoginActivity.kt
@@ -11,14 +11,15 @@ class LoginActivity : AppCompatActivity(), LoginView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
     }
 
     override fun onLoginSuccess() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onLoginFail() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
--- app/src/main/java/com/projects/enzoftware/nutricoach/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/projects/enzoftware/nutricoach/ui/register/RegisterActivity.kt
@@ -11,15 +11,16 @@ class RegisterActivity : AppCompatActivity(), RegisterView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar?.hide()
     }
 
 
     override fun onRegisterSuccess() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onRegisterFail() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 
